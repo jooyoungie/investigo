97b870b7710bde4d007eaddb52efb26c6755ee87
jooyoungie <38542850+jooyoungie@users.noreply.github.com>
2019-12-28 17:01:13 -0500

    Add files via upload

---
+++ investigo/js/step8.js
@@ -0,0 +1,12 @@
+function setup() {
+  let canvas = createCanvas(displayWidth, displayHeight);
+  frameRate(100);
+  canvas.id("sketch");
+}
+
+function draw() {
+  background(0);
+  noStroke();
+  fill(255);
+  ellipse(mouseX, mouseY, 750);
+}
